9ba8915b23256938d8a2b69c39ea21423109d0b1
Lipe Natan <lipe@Lipes-iMac.local>
2021-12-05 20:41:39 -0300

    login-working

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,8 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   { dialect: process.env.DATABASE_DIALECT,
-    dialectOptions: {connectTimeout: 1000}}
+    dialectOptions: {connectTimeout: 1000},
+    port: 3000}
 )
 
 module.exports = sequelize
